b7d54dd798a9e63d124dba190c3b0ec1097b993b
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-11-16 22:30:27 +0300

    Create hunter.cmake

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/Ivanopulopulo/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    1b8873adb40f637d36ea6faa9aa8d06760a98db2
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
